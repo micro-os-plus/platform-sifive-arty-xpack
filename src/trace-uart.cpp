947f28eee630c724bc8e220983372e0b4445dfad
Liviu Ionescu <ilg@livius.net>
2021-01-14 12:16:00 +0200

    Revert "clang-format"
    This reverts commit 4482344bee8b67e20dafbb302f196125e83c9ea4.

--- src/trace-uart.cpp
+++ src/trace-uart.cpp
@@ -36,12 +36,12 @@
 #include <micro-os-plus/device.h>
 #include <micro-os-plus/diag/trace.h>
 
-#define OS_INTEGER_TRACE_UART0_BAUD_RATE (115200)
+#define OS_INTEGER_TRACE_UART0_BAUD_RATE	(115200)
 
-#if defined(SIFIVE_E31ARTY)
+#if defined (SIFIVE_E31ARTY)
 #define UART_TXCTRL_TXEN SIFIVE_E31ARTY_UART_TXCTRL_TXEN
 #define UART_TXDATA_FULL SIFIVE_E31ARTY_UART_TXDATA_FULL
-#elif defined(SIFIVE_E51ARTY)
+#elif defined (SIFIVE_E51ARTY)
 #define UART_TXCTRL_TXEN SIFIVE_E51ARTY_UART_TXCTRL_TXEN
 #define UART_TXDATA_FULL SIFIVE_E51ARTY_UART_TXDATA_FULL
 #else
@@ -52,78 +52,77 @@
 
 namespace os
 {
-namespace trace
-{
-// ----------------------------------------------------------------------------
+  namespace trace
+  {
+    // ------------------------------------------------------------------------
 
-void
-initialize (void)
-{
-  // Set baud rate.
-  // Arty boards have the peripheral clock at half the core clock.
-  UART0->div = (riscv::core::running_frequency_hz () / 2
-                / OS_INTEGER_TRACE_UART0_BAUD_RATE)
-               - 1;
-  // Enable transmitter.
-  UART0->txctrl |= UART_TXCTRL_TXEN;
-
-  // Wait a bit to avoid corruption on the UART.
-  // (In some cases, switching to the IOF can lead
-  // to output glitches, so need to let the UART
-  // receiver time out and resynchronize to the real
-  // start of the stream.
-  volatile int i = 0;
-  while (i < 10000)
+    void
+    initialize (void)
     {
-      i++;
-    }
-}
+      // Set baud rate.
+      // Arty boards have the peripheral clock at half the core clock.
+      UART0->div = (riscv::core::running_frequency_hz () / 2
+          / OS_INTEGER_TRACE_UART0_BAUD_RATE) - 1;
+      // Enable transmitter.
+      UART0->txctrl |= UART_TXCTRL_TXEN;
 
-// ----------------------------------------------------------------------------
-
-ssize_t
-write (const void* buf, std::size_t nbyte)
-{
-  if (buf == nullptr || nbyte == 0)
-    {
-      return 0;
-    }
-
-  const char* cbuf = (const char*)buf;
-
-  for (size_t i = 0; i < nbyte; i++)
-    {
-      uint8_t ch = (*cbuf++);
-
-      if (ch == '\n')
+      // Wait a bit to avoid corruption on the UART.
+      // (In some cases, switching to the IOF can lead
+      // to output glitches, so need to let the UART
+      // receiver time out and resynchronize to the real
+      // start of the stream.
+      volatile int i = 0;
+      while (i < 10000)
         {
-          // Wait until FIFO is ready...
-          // Without handshake, should not block.
-          while ((UART0->txdata & UART_TXDATA_FULL) != 0)
-            ;
-          UART0->txdata = '\r';
+          i++;
+        }
+    }
+
+    // ------------------------------------------------------------------------
+
+    ssize_t
+    write (const void* buf, std::size_t nbyte)
+    {
+      if (buf == nullptr || nbyte == 0)
+        {
+          return 0;
         }
 
-      // Wait until FIFO is ready...
-      while ((UART0->txdata & UART_TXDATA_FULL) != 0)
-        ;
-      UART0->txdata = ch;
+      const char* cbuf = (const char*) buf;
+
+      for (size_t i = 0; i < nbyte; i++)
+        {
+          uint8_t ch = (*cbuf++);
+
+          if (ch == '\n')
+            {
+              // Wait until FIFO is ready...
+              // Without handshake, should not block.
+              while ((UART0->txdata & UART_TXDATA_FULL) != 0)
+                ;
+              UART0->txdata = '\r';
+            }
+
+          // Wait until FIFO is ready...
+          while ((UART0->txdata & UART_TXDATA_FULL) != 0)
+            ;
+          UART0->txdata = ch;
+        }
+
+      // All characters successfully sent.
+      return (ssize_t) nbyte;
     }
 
-  // All characters successfully sent.
-  return (ssize_t)nbyte;
-}
+    // ------------------------------------------------------------------------
 
-// ----------------------------------------------------------------------------
+    void
+    flush (void)
+    {
+      ; // TODO flush port
+    }
 
-void
-flush (void)
-{
-  ; // TODO flush port
-}
-
-// ----------------------------------------------------------------------------
-} /* namespace trace */
+  // --------------------------------------------------------------------------
+  } /* namespace trace */
 } /* namespace os */
 
 #endif /* defined(OS_USE_TRACE_UART0) */
